c61e3f4846430090e7c9ff735d04afe8cc4a73b8
Diego Vargas <diegovs0504@gmail.com>
2023-03-08 16:59:17 -0500

    App.tsx Basic Structure

--- react-ts-todo/src/App.tsx
+++ react-ts-todo/src/App.tsx
@@ -1,12 +1,23 @@
 import React, { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid';
+import { Todo } from "./components/"
 import './App.css'
 
 function App() {
 
+  const [todos, setTodos] = useState([]);
+
   return (
-    <div className="">
-      Hello
-    </div>
+    <div className='app__wrapper'>
+      <div className="app__wrapper-content">
+        <h1 className='headtext'>Todo App</h1>
+        <div className="app__wrapper-content_input">
+          <input className='custom__input' type="text" name="todo" id="todo" placeholder='Type a todo...' />
+          <button className='custom__button'>ADD</button>
+        </div>
+        <Todo />
+      </div>
+    </div >
   )
 }
 
